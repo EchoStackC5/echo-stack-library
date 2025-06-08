5c48f0f4a408bb979fe81a433da4f73945be7597
Abby533-alt <abigailaheto17@gmail.com>
2025-06-08 21:42:06 +0000

    add homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,10 @@
-import React from "react"
-import { Calendar } from "@/components/ui/calendar"
 
 export default function Home() {
-    const [date, setDate] = React.useState(new Date())
+    
     
     return (
         <section>
-            <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                className="rounded-lg border"
-            />
+            <h1>Welcome to EchoStack</h1>
         </section>
     )
 }
