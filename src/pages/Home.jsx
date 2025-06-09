50a4707958535280b60309029a7f9e4b599f886c
Abby533-alt <abigailaheto17@gmail.com>
2025-06-09 11:01:24 +0000

    added a heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <section>
             <h1>Welcome to EchoStack</h1>
+            <p>Where all categories of books you want can be found</p>
         </section>
     )
 }
