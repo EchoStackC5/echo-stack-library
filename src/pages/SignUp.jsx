221cadfc2fa0aa865e02c2635cd7e7e923bbceab
Ayimaah <ayimaahtheodora2022@gmail.com>
2025-06-09 14:17:18 -0700

    book details

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,10 +2,7 @@ export default function SignUp(){
     return(
        
         <>
-         <p>Sign Up here....</p>
-            <button className="bg-blue-950 rounded-2xl text-white">
-                <a href="">tap here</a>
-            </button>
+         
         </>
         
     )
--- src/pages/dashboard/BookDetails.jsx
+++ src/pages/dashboard/BookDetails.jsx
@@ -2,7 +2,7 @@ export default function BookDetails(){
     return(
         <section>
             <p>what book info would you like to know?</p>
-            <button></button>
+            <h1>welcom</h1>
         </section>
     )
 }
