fdca41d5969ff4e2cde04605657fb1a7ebb9e9bf
Abby533-alt <abigailaheto17@gmail.com>
2025-06-09 11:19:38 +0000

    added a heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     
     return (
         <section>
-            <h1>Welcome to EchoStack</h1>
+            <h1 className="font-bold text-3xl">Welcome to EchoStack</h1>
             <p>Where all categories of books you want can be found</p>
         </section>
     )
