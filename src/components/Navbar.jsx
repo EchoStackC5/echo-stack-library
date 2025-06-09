9b85983df739c7c99f2e949623d8dbe45300713e
Abby533-alt <abigailaheto17@gmail.com>
2025-06-09 14:22:47 +0000

    adding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 export default function Navbar(){
     return(
-        <section>
-            <p>This is Navbar</p>
-        </section>
+            <nav className="Flex">
+                <h1>EchoBooks</h1>
+                <div>
+                    <span>Home</span>
+                    <span>About Us</span>
+                    <span>Features</span>
+                    <span>Contact</span>
+                </div>
+                <div className=" flex space-x-5 text-white text-xs">
+                    <button className="h-8 w-20 bg-primary rounded-2xl">Sign Up</button>
+                    <button className="h-8 w-20 bg-primary rounded-2xl">Login</button>
+                </div>
+            </nav>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-
+import Navbar from "@/components/Navbar"
 export default function Home() {
     
     
     return (
-        <section className="flex justify-around py-4">
-            <h1 className="font-bold">EchoStack</h1>
-            <div className="space-x-5 text-white">
-                <button className="bg-amber-700 h-8 w-20 rounded-md">Login</button>
-                <button className="bg-amber-700 h-8 w-20 rounded-md">Signup</button>
-            </div>
+        <>
+        <Navbar/>
+        <section>
+            
         </section>
+        </>
+        
     )
 }
