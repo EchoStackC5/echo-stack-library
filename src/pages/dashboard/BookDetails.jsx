61320680daa9ec7b8e65d618577225ddf2f34350
Ayimaah <ayimaahtheodora2022@gmail.com>
2025-06-08 22:00:15 -0700

    added book details

--- src/pages/dashboard/BookDetails.jsx
+++ src/pages/dashboard/BookDetails.jsx
@@ -1,7 +1,7 @@
 export default function BookDetails(){
     return(
         <section>
-            <p>Book Details</p>
+            <p>what book info would you like to know?</p>
         </section>
     )
 }
