4499c8195da6d605261c0876467290f69d0458e1
Gloria Dedo <gloriadedo100@gmail.com>
2025-06-10 03:36:22 -0700

    added data

--- src/constants/BooksAvailable.jsx
+++ src/constants/BooksAvailable.jsx
@@ -1,5 +1,7 @@
 // const { title } = require("process");
 
+// import { au } from "react-router/dist/development/register-DCE0tH5m";
+
 const AvailableBooksConstants = [
      {
         imageUrl: 'https://m.media-amazon.com/images/I/71QKQ9mwV7L._SL1500_.jpg',
@@ -12,7 +14,24 @@ const AvailableBooksConstants = [
         imageUrl: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTEWxSmnpElsT0BstvoMf_YaAkU3mwNKiWyCVfRQCquLp9EOBVj",
         title: "Nearly All the Men in Lagos are Mad",
         author: "Damilare Kuku",
-        bgColor: "bg-yellow-300"
+        bgColor: "bg-yellow-300",
+        genre: "Fiction" || "Contemporary Fiction" || "Satire"
+    }
+    ,
+    {
+        imageUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR1lqbRrXj-WJ7fur2a7w7LMEax69pX6_w7rie8U_k5Qik-VB3h",
+        title: "Rich Dad Poor Dad",
+        bgColor: "bg-purple-300",
+        author: "Robert Kiyosaki",
+        genre: "Finance" || "Personal Finance" || "Investing"
+    },
+    {
+        imageUrl: "https://i.ebayimg.com/images/g/FOUAAOSwNrZnvXyC/s-l1600.webp",
+        title: "Atomic Habits",
+        bgColor: "bg-blue-700",
+        author: "James Clear",
+        genre: "Self-Help" || "Personal Development" || "Productivity"
+
     }
 ]
 export default AvailableBooksConstants;
