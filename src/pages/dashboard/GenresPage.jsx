727891e742516b3fd8d99a25eb27f786c4660bdc
Gloria Dedo <gloriadedo100@gmail.com>
2025-06-13 09:25:08 -0700

    fix: fiexed navigations moving

--- src/pages/dashboard/GenresPage.jsx
+++ src/pages/dashboard/GenresPage.jsx
@@ -3,7 +3,7 @@ import GenreBarChart from "./GenreBarChart";
 // import { chartConfig } from "./GenreBarChart";
 export default function GenresPage() {
   return (
-    <div className="flex flex-col gap-8 md:gap-15 p-4 md:p-6">
+    <div className="flex flex-col gap-8 md:gap-15 p-4 md:p-6 ">
         <div className="font-font-satoshi space-y-4">
         <h1 className=" text-lg md:text-2xl font-medium text-darkestHeading ">Manage Genres</h1>
         <p className="text-muted-foreground text-sm md:text-lg ">Manage your book genres here. You can add, edit, or delete genres as needed.</p>
